0f8d543d167dc57e11e26362adf9590abbb8c12a
jch108 <82850028+jch108@users.noreply.github.com>
2022-11-16 14:56:27 -0500

    Delete Block.py

--- TrainModel/Block.py
+++
@@ -1,7 +0,0 @@
-class Block:
-
-    def __init__(self,name, station, block_length, grade):
-        self.name = name                    #name of block
-        self.station = station              #name of station, "" if no station
-        self.block_length = block_length    #length of block, meters
-        self.grade = grade                  #percentage. float
