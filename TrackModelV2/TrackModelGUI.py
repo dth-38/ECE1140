9d56214d457e827d403d41091e1c830740ae3a59
Dylan Himes <DTH38@pitt.edu>
2022-12-13 15:16:13 -0500

    fixed a few small display issues in track model

--- TrackModelV2/TrackModelGUI.py
+++ TrackModelV2/TrackModelGUI.py
@@ -2,8 +2,8 @@ from PyQt5.QtWidgets import QMainWindow, QTableWidget, QTabWidget, QVBoxLayout, 
 from PyQt5.QtGui import QColor
 from Signals import signals
 
-HEIGHT = 1080
-WIDTH = 1080
+HEIGHT = 1000
+WIDTH = 1000
 X_POS = 0
 Y_POS = 0
 
@@ -40,7 +40,7 @@ class TrackModelGUI(QMainWindow):
             new_line = QTableWidget()
 
             new_line.setColumnCount(13)
-            col_headers = ["Section","Occupied","Authority","Commanded Spd.","Station","Beacon","Switch","Light","Gate","Length","Grade","Underground","Max Speed"]
+            col_headers = ["Section","Occupied","Authority (Blocks)","Commanded Spd. (mph)","Station","Beacon","Switch","Light","Gate","Length (ft)","Grade","Underground","Max Speed"]
             new_line.setHorizontalHeaderLabels(col_headers)
 
             row_headers = []
@@ -65,7 +65,11 @@ class TrackModelGUI(QMainWindow):
                 new_line.setItem(block,1, occ)
 
                 #create authority item
-                new_line.setItem(block,2, QTableWidgetItem(str(track[line][block].authority)))
+                auth = track[line][block].authority
+                if auth == -1:
+                    new_line.setItem(block,2, QTableWidgetItem(""))
+                else:
+                    new_line.setItem(block,2, QTableWidgetItem(str(auth)))
 
                 #create commanded speed item
                 new_line.setItem(block,3, QTableWidgetItem(str(track[line][block].commanded_speed)))
@@ -110,7 +114,7 @@ class TrackModelGUI(QMainWindow):
                 new_line.setItem(block,8, g)
 
                 #create length item
-                l_b = QTableWidgetItem(str(track[line][block].LENGTH))
+                l_b = QTableWidgetItem(str(round(track[line][block].LENGTH / 0.3408, 2)))
                 new_line.setItem(block,9,l_b)
 
                 #create grade item
@@ -144,7 +148,11 @@ class TrackModelGUI(QMainWindow):
             self.table_widgets[line].item(block,1).setBackground(self.WHITE)
 
     def update_authority(self, line, block):
-        self.table_widgets[line].item(block,2).setText(str(self.get_track()[line][block].authority))
+        auth = self.get_track()[line][block].authority
+        if auth == -1:
+            self.table_widgets[line].item(block,2).setText("")
+        else:
+            self.table_widgets[line].item(block,2).setText(str(auth))
 
     def update_spd(self, line, block):
         self.table_widgets[line].item(block,3).setText(str(self.get_track()[line][block].commanded_speed))
